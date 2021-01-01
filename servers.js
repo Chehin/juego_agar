03dd2934752e3bee339fc995b6b406c9fcd447af
Juan Chehin SGM <chehinjuan@gmail.com>
2021-01-01 19:22:07 -0300

    CORS

--- servers.js
+++ servers.js
@@ -2,6 +2,7 @@
 // Agar.io clone
 const express = require('express');
 const app = express();
+const cors = require('cors');
 app.use(express.static(__dirname + '/public'));
 const socketio = require('socket.io');
 // const expressServer = app.listen(5000);
@@ -12,9 +13,21 @@ const io = socketio(expressServer);
 const helmet = require('helmet')
 app.use(helmet());
 console.log("Express and socketio are listening on port ");
-
 // App organization
 module.exports = {
     app,
     io
 }
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+// CORS
+// app.use(cors(
+//     config.application.cors.server
+// ));
