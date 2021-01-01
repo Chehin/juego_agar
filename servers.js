2d6220001e408dfcca3257a163c3b0a6d6e69a70
Juan Chehin SGM <chehinjuan@gmail.com>
2021-01-01 19:00:34 -0300

    Nuevo cambio en puerto

--- servers.js
+++ servers.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/public'));
 const socketio = require('socket.io');
-const expressServer = app.listen(5000);
-console.log('Prueba 2021');
-// const expressServer = app.listen(process.env.PORT || 5000); // Para heroku
+// const expressServer = app.listen(5000);
+// console.log('Prueba 2021');
+const expressServer = app.listen(process.env.PORT || 5000); // Para heroku
 // const expressServer = app.listen(process.env.PORT || 8080);
 const io = socketio(expressServer);
 const helmet = require('helmet')
